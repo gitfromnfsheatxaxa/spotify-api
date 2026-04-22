45764bb8372395e11449292acfd8bb7f1c4ca41d
gitfromnfsheatxaxa <xadasad67@gmail.com>
2026-04-22 14:44:51 +0500

    aa

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,12 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals.js";
-import nextTs from "eslint-config-next/typescript.js";
-
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
-
-export default eslintConfig;
+// Simple ESLint config for Next.js
+export default [
+  {
+    ignores: ['.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
+  },
+  {
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+    rules: {
+      // Add any custom rules here
+    },
+  },
+];
